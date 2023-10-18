34bc2be695a392621ad5410893665da2e8e63708
aryank09 <148300045+aryank09@users.noreply.github.com>
2023-10-17 21:40:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-#importing necessary modules for the runnnig of the program
+#importing necessary modules
 import tkinter
 import mysql.connector
 import admin
 import employee
 import sys
 
-#establishing mysql connection
+#establishing connection
 mydb=mysql.connector.connect(host='localhost',user='root',password='root',database='crm')
 mycursor=mydb.cursor()
+#running the comands
 mycursor.execute('create table if not exists admin(username varchar(10) primary key,password varchar(10));')
 mycursor.execute('create table if not exists employee(username varchar(10) primary key,password varchar(10));')
+#saving the changes
 mydb.commit()
 
+#function the exit program
 def exit():
     sys.exit()
 
-#running the user nterface
+#creating buttons for running the program and connection them to necessary functions
 m=tkinter.Tk()
 mbutton_1=tkinter.Button(text = "Admin" ,command=admin.admin_account).pack()
 mbutton_2=tkinter.Button(text = "Employee",command=employee.employee_account).pack()
